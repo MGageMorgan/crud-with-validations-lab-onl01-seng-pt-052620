592807427c428971298ecb839d058797e059dbb9
M. Gage Morgan <gage@gages.blog>
2020-10-14 14:05:52 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,12 +1,31 @@
-# I'm behind in my cohort. I know what Avi wants us to do, I can tell you I
-# am out of time to mess around with RailsGuides - I get one day off a week if that tells you anything. 
-# My options are AAQ, and, failing that, ripping the code out of someone else's copy 
-# just for the sake of getting these tests to pass.
+# Ruby files can support more than one class, and I'm lazy
+# class CurrentYearValidator < ActiveModel::Validator
+#   def validate(record)
+#     current_year = DateTime.now.year
+#     if record.release_year > current_year
+#       record.errors[:release_year] << 'Year cannot be in the future!'
+#     end
+#   end
+# end
+
 class Song < ApplicationRecord
+  # validate :release_year_cannot_be_in_future
+
+  # def release_year_cannot_be_in_future
+  #   current_year = DateTime.now.year
+  #   if self.release_year > current_year
+  #     errors.add(:release_year, "Year cannot be in the future!")
+  #   end
+  # end
+
+  # include ActiveModel::Validations
+  # validates_with CurrentYearValidator
+
   validates :title, presence: true
 
   # Cannot be repeated by the same artist in the same year
-  # How? Curriculum doesn't cover this, I'm out of time, I have to move on.
+  validates :title, uniqueness: { scope: :released,
+    message: "Cannot be repeated by the same artist in the same year" }
 
   validates :released, inclusion: { in: %w(true false),
     message: "%{value} must be true or false" }
@@ -15,6 +34,10 @@ class Song < ApplicationRecord
   # Optional if released is false
   # Must not be blank if released is true
   # Must be less than or equal to the current year
+  current_year = DateTime.now.year
+  with_options if: :released == true && :release_year < current_year do |release|
+    release.validates :release_year, presence: true
+  end
   
   validates :artist_name, presence: true
 end
